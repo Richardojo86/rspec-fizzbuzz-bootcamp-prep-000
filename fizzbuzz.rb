bfdf690bb4ab2b8c0075011fe4a68a06e529a2d6
richardojo04@gmail.com <richardojo04@gmail.com>
2018-04-24 21:41:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,9 @@ def fizzbuzz(number)
     return "FizzBuzz"
   elsif number % 3 == 0 
    return "Fizz"
- elsif number % 5 == 0 
+  elsif number % 5 == 0 
     return "Buzz"
+  else 
+    return nil
  end
 end
